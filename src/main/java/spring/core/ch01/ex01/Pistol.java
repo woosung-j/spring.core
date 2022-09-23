3cfe482aacaee8aa01e0ffce0f0adf501e8d1410
Woosung-j <developer.jws@gmail.com>
2022-09-23 16:56:56 +0900

    Initial commit

---
+++ src/main/java/spring/core/ch01/ex01/Pistol.java
@@ -0,0 +1,7 @@
+package spring.core.ch01.ex01;
+
+public class Pistol implements Gun {
+	public void fire() {
+		System.out.println("권총 탕탕.");
+	}
+}
---
+++ src/main/java/spring/core/ch01/ex01/Rifle.java
@@ -0,0 +1,7 @@
+package spring.core.ch01.ex01;
+
+public class Rifle implements Gun {
+	public void fire() {
+		System.out.println("소총 탕탕.");
+	}
+}

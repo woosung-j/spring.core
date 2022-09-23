88fdb37b06b1c62c6c5ae700c0eb648beec09c7e
Woosung-j <developer.jws@gmail.com>
2022-09-23 17:58:55 +0900

    Component - Autowired

---
+++ src/main/java/spring/core/ch01/ex05/Shooter.java
@@ -0,0 +1,20 @@
+package spring.core.ch01.ex05;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Shooter {
+	@Autowired @Nullable private Gun pistol;
+	@Autowired @Nullable private Gun rifleGun;
+	
+	public void fire() {
+		try {
+			pistol.fire();
+			rifleGun.fire();
+		} catch (Exception e) {
+			
+		}
+	}
+}	
